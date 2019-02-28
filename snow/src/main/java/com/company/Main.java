615131e299f7a5ab3f6801b071644af31818e058
JeffRisberg <jrisberg@aol.com>
2019-02-28 15:43:44 -0800

    added use of another library in snow, to check include/exclude

--- snow/src/main/java/com/company/Main.java
+++ snow/src/main/java/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 import com.company.models.Item;
 import com.company.stores.ItemStore;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.List;
 
 @Slf4j
@@ -11,13 +15,26 @@ public class Main {
 
   public static void main(String[] args) {
 
-    ItemStore itemStore = new ItemStore();
+    try {
+      ItemStore itemStore = new ItemStore();
 
-    List<Item> list = itemStore.readAllItems();
-    for (Item listItem : list) {
-      System.out.println(listItem);
+      List<Item> list = itemStore.readAllItems();
+      for (Item listItem : list) {
+        System.out.println(listItem);
+      }
+
+      Reader in = new FileReader("people.csv");
+      Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+      for (CSVRecord record : records) {
+        String lastName = record.get("Last Name");
+        String firstName = record.get("First Name");
+
+        System.out.println(firstName + " " + lastName);
+      }
+
+      log.debug("Welcome to Example 1");
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-
-    log.debug("Welcome to Example 1");
   }
 }
