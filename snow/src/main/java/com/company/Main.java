d302a22a20fe889add4fba07bc368e23beaeba45
JeffRisberg <jrisberg@aol.com>
2019-02-27 19:16:52 -0800

    updated dependencies

--- snow/src/main/java/com/company/Main.java
+++ snow/src/main/java/com/company/Main.java
@@ -1,11 +1,25 @@
 package com.company;
 
+import com.company.models.Item;
+import com.company.services.ItemService;
+import com.company.stores.ItemStore;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @Slf4j
 public class Main {
 
   public static void main(String[] args) {
+
+    ItemStore itemStore = new ItemStore();
+    ItemService itemService = new ItemService(itemStore);
+
+    List<Item> list = itemStore.readAllItems();
+    for (Item listItem : list) {
+      System.out.println(listItem);
+    }
+
     log.debug("Welcome to Example 1");
   }
 }
